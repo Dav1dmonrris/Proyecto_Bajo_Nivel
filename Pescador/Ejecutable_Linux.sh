7b8448588ad1ba788b6896978c335bfeea7e31db
dav1dmonrris <davidangmones@gmail.com>
2025-11-02 22:51:48 -0600

    Archivo Ejecutable

---
+++ Pescador/Ejecutable_Linux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Script para Archivo ejecutable en Linux
+# - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+#Comando para limpiar los archivos objeto y binario viejos
+rm main.o Juego
+
+#Comando para convertir un archivo en objeto
+g++ -c ./Pescador/main.cpp
+
+#Comando para convertir un archivo objeto en binario
+g++ main.o -o Juego -lsfml-graphics -lsfml-window -lsfml-system 
